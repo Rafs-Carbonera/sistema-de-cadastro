ab1ca597171b8f9a3d6c04aec4ca29006cbc3688
Rafael <carbonera.rafael@gmail.com>
2022-08-21 09:37:33 -0300

    voltando para alguns commits atras

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-// alteração que teria que ser feita por outra pessoa 
-console.log("Eu não tenho amigos acordados para me ajudar a fazer esse teste!")
-console.log("Enviando para o Git-Hub")
