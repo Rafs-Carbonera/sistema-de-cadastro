8978513ddfe59e4bdc882997f12bfb60adbe342d
Rafael <carbonera.rafael@gmail.com>
2022-08-21 09:27:44 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// alteração que teria que ser feita por outra pessoa 
+console.log("Eu não tenho amigos acordados para me ajudar a fazer esse teste!")
+console.log("Enviando para o Git-Hub")
